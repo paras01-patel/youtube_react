6e9d098d9902c0baa3cc41f82a68efef58cf1c59
paras01-patel <paraschandrawanshi649@gmail.com>
2026-03-22 13:27:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
-function App(){
-  let a =20;
-  function cha(){
-    a++;
-    console.log(a);
-  }
+// import { useState } from "react"
 
-  function the(th){
-    console.log(th.target.value)
+// import { use, useState } from "react"
+
+// function App(){
+// const [num,write]=useState(0)
+
+
+// function inn(){
+//   write(num+1)
+// }
+
+// function dee(){
+//   write(num-1)
+// }
+
+// function deee(){
+//   write(num+5)
+// }
+//   return (
+//     <>
+//   <h1>{num}</h1>
+//   <button onClick={inn}>increase</button>
+//   <button onClick={dee}>decrease</button>
+//   <button onClick={deee}>increase by 5 </button>
+  
+
+//     </> 
+//   )
+// }
+// export default App
+
+import { use, useState } from "react"
+function App(){
+
+
+  let [read,write]=useState({user:'paras',age:22})
+  function chh(){
+    let neww={...read}
+neww.user='PARAS'
+neww.age=21
+write(neww)
   }
-  return (
+  return(
     <>
-    <h1>this is{a}</h1>
-    <button onDoubleClick={cha} className="mt-10 ml-20 bg-black ">click</button>
-    <input type="text" placeholder="name" onChange={the} />
+    <h1>hii i am {read.user} and i am {read.age} old </h1>
+    <button onClick={chh}> change</button>
+    
     </>
   )
 }
